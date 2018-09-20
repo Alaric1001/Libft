898f02084d848dd9da3ea542b239d65c7e620a2f
Alaric Senat <asenat@student.42.fr>
2018-09-20 15:07:13 +0200

    Updating `gen_make_sources' script: 	- Rewritten in python (3.7 needed) 	- we can now choose the exec name of the object files

--- color/Sources.mk
+++ color/Sources.mk
@@ -1,4 +1,5 @@
 ## Auto generated make tool, don't edit manually.
 
-OBJ_FILES_color  :=  ft_getcolor.o
-OBJECTS   += $(patsubst %, color/$(OBJECT_DIR)/%, $(OBJ_FILES_color))
+OBJ_FILES_color := ft_getcolor.o
+
+OBJECTS += $(patsubst %, color/$(OBJECT_DIR)/%, $(OBJ_FILES_color))
